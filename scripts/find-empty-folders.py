4dc7e3811a30baf0750fda8d3635c6c696046f20
Sergii Lovygin <sergey.lovygin@yahoo.com>
2021-08-28 21:03:55 +0300

    add stub for find-empty-folders script

--- scripts/find-empty-folders.py
+++ scripts/find-empty-folders.py
@@ -1,26 +1,73 @@
-import argparse
-import os
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+from sys import exit
 
 
-def get_directory_list(root_dir):
-    return []
+def get_empty_directories(root_dir: Path, empty_files=False) -> list[Path]:
+    result = []
+
+    dirs = [path for path in root_dir.iterdir() if path.is_dir()]
+    if not dirs:
+        files = [path for path in root_dir.iterdir() if path.is_file()]
+        if not files:
+            result.append(root_dir)
+        elif empty_files:
+            files_size = sum(file.stat().st_size for file in files)
+            if not files_size:
+                result.append(root_dir)
+
+    for child in dirs:
+        result.extend(get_empty_directories(child, empty_files))
+
+    return result
 
 
-def parse_options():
-    parser = argparse.ArgumentParser()
+def print_dirs(dirs: list[Path]):
+    print(*dirs, sep='\n')
+
+
+def remove_dirs(dirs: list[Path]):
+    actions = [f'remove "{path}"' for path in dirs]
+    print(*actions, sep='\n')
+
+
+def get_action(name: str) -> callable:
+    actions = {
+        'print':  print_dirs,
+        'remove': remove_dirs
+    }
+
+    return actions[name] if name in actions else print_dirs
+
+
+def parse_options() -> Namespace:
+    parser = ArgumentParser()
     parser.add_argument('-r', '--root-dir',
-                        type=str, default=os.getcwd(),
+                        type=Path, required=True,
                         help='root directory ')
     parser.add_argument('-f', '--empty-files',
-                        type=bool, default=True,
+                        type=bool, default=False,
                         help='treat empty files as absent.')
+    parser.add_argument('-a', '--action',
+                        type=str, default='print',
+                        help='action will be performed for each empty directory:\n'
+                             '\tprint (default)'
+                             '\tremove')
 
     return parser.parse_args()
 
 
-if __name__ == "__main__":
+def main():
     options = parse_options()
 
-    root_dir = os.path.abspath(options.root_dir)
-    if not os.path.exists(root_dir):
+    root_dir = options.root_dir.absolute()
+    if not root_dir.exists():
         print("the root directory is not set or doesn't exists")
+
+    empty_dirs = get_empty_directories(root_dir, options.empty_files)
+    action = get_action(options.action)
+    action(empty_dirs)
+
+
+if __name__ == '__main__':
+    exit(main())
