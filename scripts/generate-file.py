d76f63c7f01f32530454e36d9acafe4f7032f8bf
Sergii Lovygin <sergey.lovygin@yahoo.com>
2021-08-28 19:54:55 +0300

    improve code quality a bit

--- scripts/generate-file.py
+++ scripts/generate-file.py
@@ -7,26 +7,26 @@ from string import ascii_uppercase, digits
 
 def get_file_size_in_bytes(size_string):
     multiplier = {
-        "gb": 1024 * 1024 * 1024,
-        "mb": 1024 * 1024,
-        "kb": 1024
+        'gb': 1024 * 1024 * 1024,
+        'mb': 1024 * 1024,
+        'kb': 1024
     }
 
     result = search(r'(?P<number>^\d+\.?\d+)(?P<multiplier>gb|mb|kb$)?', size_string, IGNORECASE)
-    if result is not None:
+    if result:
         number = float(result.group('number'))
-        ratio = 1 if result.group('multiplier') is None else multiplier[result.group('multiplier').lower()]
+        ratio = 1 if not result.group('multiplier') else multiplier[result.group('multiplier').lower()]
         return int(number * ratio)
     return 0
 
 
-def generate_random_string(size=1024, chars=ascii_uppercase + digits):
-    return ''.join(choice(chars) for _ in range(size))
+def generate_random_string(size=1024, allowed_chars=ascii_uppercase+digits):
+    return ''.join(choice(allowed_chars) for _ in range(size))
 
 
 def generate_file(path, size, line_length=1024):
     with open(path, 'w') as file:
-        for i in range(size // line_length):
+        for _ in range(size // line_length):
             generated_line = generate_random_string(line_length) + '\n'
             file.write(generated_line)
 
@@ -50,19 +50,22 @@ def parse_options():
     return parser.parse_args()
 
 
-if __name__ == "__main__":
+def main():
     options = parse_options()
 
-    file_path = Path(options.file_path).absolute()
-    if not file_path:
+    file_path = Path(options.file_path).absolute() if options.file_path else default_file_location()
+    if not options.file_path:
         print('Option <file-path> is empty. The default value location will be used.')
-        file_path = default_file_location()
 
     file_size = get_file_size_in_bytes(options.file_size)
-    if file_size is 0:
-        print(f'Invalid file size was requested (file-size = {options.file_size}).')
+    if not file_size:
+        print(f'Invalid file size was requested (file-size={options.file_size}).')
         print(f'Use "{Path(__file__).name} -h" to see allowed options.')
     else:
         print(f'The file of {options.file_size} will be generated at "{file_path}".')
         generate_file(file_path, file_size, options.line_size)
         print('The generation is finished.')
+
+
+if __name__ == '__main__':
+    main()
