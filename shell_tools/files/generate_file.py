d76e08d8c10db3b9ee90492a4163a10345c2e61a
Sergii Lovygin <sergey.lovygin@yahoo.com>
2022-10-22 23:36:17 +0300

    replace `argparse` with `click` for `shell_utils.files` subpackage

--- shell_tools/files/generate_file.py
+++ shell_tools/files/generate_file.py
@@ -1,14 +1,32 @@
-from argparse import ArgumentParser
-from argparse import Namespace
 from pathlib import Path
 from random import choice
 from re import IGNORECASE
 from re import search
 from string import ascii_uppercase
 from string import digits
+from sys import argv
+
+from click import command
+from click import option
 
 
-def get_file_size_in_bytes(size_string: str) -> int:
+def generate_random_string(size: int = 1024, allowed_chars: str = ascii_uppercase + digits) -> str:
+    return "".join(choice(allowed_chars) for _ in range(size))
+
+
+def generate_file(path: Path, size: int, line_length: int = 1024) -> None:
+    with open(path, "w") as file:
+        full_rows_count, remains = divmod(size, line_length)
+        for _ in range(full_rows_count):
+            generated_line = generate_random_string(line_length) + "\n"
+            file.write(generated_line)
+
+        if remains:
+            generated_line = generate_random_string(remains) + "\n"
+            file.write(generated_line)
+
+
+def determine_file_size(size_string: str) -> int:
     multiplier = {"gb": 1024 * 1024 * 1024, "mb": 1024 * 1024, "kb": 1024}
 
     result = search(r"(?P<number>^\d+\.?\d+)(?P<multiplier>gb|mb|kb$)?", size_string, IGNORECASE)
@@ -19,50 +37,39 @@ def get_file_size_in_bytes(size_string: str) -> int:
     return 0
 
 
-def generate_random_string(size: int = 1024, allowed_chars: str = ascii_uppercase + digits) -> str:
-    return "".join(choice(allowed_chars) for _ in range(size))
-
-
-def generate_file(path: Path, size: int, line_length: int = 1024) -> None:
-    with open(path, "w") as file:
-        for _ in range(size // line_length):
-            generated_line = generate_random_string(line_length) + "\n"
-            file.write(generated_line)
-
-
 def default_file_location() -> Path:
     return Path("generated-file.txt").absolute()
 
 
-def parse_options() -> Namespace:
-    parser = ArgumentParser()
-    parser.add_argument(
-        "--file-path", "-fp", type=str, default=default_file_location(), help="Location for generated file."
-    )
-    parser.add_argument(
-        "-fs",
-        "--file-size",
-        type=str,
-        default="10Mb",
-        help="Required file size in bytes. Note you can use one next suffixes [kb, mb, gb].",
-    )
-    parser.add_argument("-ls", "--line-size", type=int, default=100, help="Length of line/chunk.")
-
-    return parser.parse_args()
+def get_program_name() -> str:
+    return Path(argv[0]).stem
 
 
-def cli() -> None:
-    options = parse_options()
+@command()
+@option("--path", type=Path, default=default_file_location(), help="Location for generated file.")
+@option("--size", type=str, default="10mb", help="Required file size in bytes (optional suffixes: kb, mb, gb).")
+@option("--line-size", type=int, default=64, help="Length of line/chunk.")
+def cli(path: Path, size: str, line_size: int) -> None:
+    path = path.absolute()
+    if path.is_dir():
+        print(f"Script aborted: '{path}' exists and it's directory")
+        return
 
-    file_path = Path(options.file_path).absolute() if options.file_path else default_file_location()
-    if not options.file_path:
-        print("Option <file-path> is empty. The default value location will be used.")
+    if path.is_file():
+        answer = input(f"file '{path}' already exists. override? [yes/no]: ")
+        if not answer.lower().startswith("y"):
+            return
 
-    file_size = get_file_size_in_bytes(options.file_size)
+    file_size = determine_file_size(size)
     if not file_size:
-        print(f"Invalid file size was requested (file-size={options.file_size}).")
-        print(f'Use "{Path(__file__).name} -h" to see allowed options.')
-    else:
-        print(f'The file of {options.file_size} will be generated at "{file_path}".')
-        generate_file(file_path, file_size, options.line_size)
-        print("The generation is finished.")
+        print(f"Invalid file size was requested (file-size={size}).")
+        print(f"Use '{get_program_name()} --help' to see allowed options.")
+        return
+
+    print(f"The file of {size} will be generated at '{path}'.")
+    generate_file(path, file_size, line_size)
+    print("The generation is finished.")
+
+
+if __name__ == "__main__":
+    cli()
