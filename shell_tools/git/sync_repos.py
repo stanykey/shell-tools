adc4b529df42d9bb79a89378f45478c085f75695
Sergii Lovygin <sergey.lovygin@yahoo.com>
2022-10-31 14:58:23 +0200

    add new option for the `sync-repos`

--- shell_tools/git/sync_repos.py
+++ shell_tools/git/sync_repos.py
@@ -1,4 +1,3 @@
-"""Update recursively all repos."""
 from pathlib import Path
 from subprocess import PIPE
 from subprocess import Popen
@@ -13,23 +12,31 @@ def gather_repos(root_dir: Path, recursive: bool = False) -> tuple[Path, ...]:
     return tuple(folder.parent for folder in root_dir.glob(pattern))
 
 
-def update_repo(repo: Path) -> list[str]:
-    process = Popen(["git", "-C", str(repo), "pull"], stdout=PIPE)
-    stdout, _ = process.communicate()
-    return stdout.decode("utf-8").strip().split("\n")
+def execute_command(*arguments: str) -> list[str]:
+    with Popen(arguments, stdout=PIPE) as shell:
+        stdout, _ = shell.communicate()
+        return stdout.decode("utf-8").strip().split("\n")
+
+
+def update_repo(repo: Path, submodules: bool) -> list[str]:
+    output = execute_command("git", "-C", str(repo), "pull")
+    if submodules:
+        output += execute_command("git", "-C", str(repo), "submodule", "update", "--init", "--recursive")
+    return output
 
 
 @command()
-@argument("root-dir", type=Path, default=Path.cwd(), help="Repos root dir")
+@argument("root-dir", type=Path, default=Path.cwd())
 @option("-r", "--recursive", is_flag=True, help="Search git repos recursively")
-def cli(root_dir: Path, recursive: bool) -> None:
+@option("--submodules/--no-submodules", is_flag=True, help="Update or not submodules")
+def cli(root_dir: Path, recursive: bool, submodules: bool) -> None:
     root_dir = root_dir.absolute()
     suffix = "recursively " if recursive else ""
     print(f"Scanning '{root_dir}' for git repositories {suffix}...")
 
     repos = gather_repos(root_dir, recursive)
     for repo in repos:
-        update_repo(repo)
+        update_repo(repo, submodules)
         print(f"{repo.relative_to(root_dir)} was synced")
 
     input("Press Enter to exit...")
