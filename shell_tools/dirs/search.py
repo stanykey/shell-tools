620de63e71eb36863f20c73679174e428d99e86c
Sergii Lovygin <sergey.lovygin@yahoo.com>
2023-04-14 23:13:10 +0300

    do maintenance for the `dirs.search` module
    - did some simplifications to improve readability
    - added some tests
    - added small descriptions

--- shell_tools/dirs/search.py
+++ shell_tools/dirs/search.py
@@ -1,19 +1,20 @@
 from pathlib import Path
 
 
-def find_empty_dirs(root_dir: Path, ignore_empty_files: bool = False) -> list[Path]:
+def find_empty_dirs(root: Path, ignore_empty_files: bool = False) -> list[Path]:
+    """Find all empty dirs recursively from given `root` directory."""
     result = []
 
-    items = tuple(path for path in root_dir.iterdir())
-    dirs = tuple(filter(lambda path: path.is_dir(), items))
+    items = [path for path in root.iterdir()]
+    dirs = [path for path in items if path.is_dir()]
     if not dirs:
-        files = tuple(filter(lambda path: path.is_file(), items))
+        files = [path for path in items if path.is_file()]
         if not files:
-            result.append(root_dir)
+            result.append(root)
         elif ignore_empty_files:
             non_empty = any(file.stat().st_size > 0 for file in files)
             if not non_empty:
-                result.append(root_dir)
+                result.append(root)
 
     for child_dir in dirs:
         result.extend(find_empty_dirs(child_dir, ignore_empty_files))
---
+++ tests/dirs/search.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from unittest import main
+from unittest import TestCase
+
+from shell_tools.dirs.search import find_empty_dirs
+
+
+class SearchTestCase(TestCase):
+    def test_find_empty_dirs(self) -> None:
+        with TemporaryDirectory() as dir_name:
+            root = Path(dir_name)
+            result = find_empty_dirs(root)
+            self.assertEqual(1, len(result))
+            self.assertEqual(root, result[0])
+
+            (root / "one").mkdir()
+            (root / "two").mkdir()
+            result = find_empty_dirs(root)
+            self.assertEqual(2, len(result))
+            self.assertTrue(all(child.parent.stem == root.stem for child in result))
+
+            file_path = root / "one" / "file"
+            file_path.touch()
+            result = find_empty_dirs(root)
+            self.assertEqual(1, len(result))
+            self.assertEqual("two", result[0].stem)
+            self.assertEqual(2, len(find_empty_dirs(root, ignore_empty_files=True)))
+
+            file_path.write_bytes(b"test data")
+            self.assertEqual(1, len(find_empty_dirs(root, ignore_empty_files=True)))
+
+
+if __name__ == "__main__":
+    main()
