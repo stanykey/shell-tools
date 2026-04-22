9834882f486e70acfc4547e25929e9b5facc3183
Sergii Lovygin <sergey.lovygin@yahoo.com>
2026-04-22 23:02:21 +0300

    refactor: clarify directory variable names and improve docstrings

--- shell_tools/dirs/search.py
+++ shell_tools/dirs/search.py
@@ -3,22 +3,22 @@ from pathlib import Path
 
 
 def find_empty_dirs(root: str | PathLike[str], ignore_empty_files: bool = False) -> list[Path]:
-    """Find all empty dirs recursively from given `root` directory."""
-    root = Path(root)
-    if not root.is_dir():
+    """Find all empty dirs recursively from a given `root` directory."""
+    start_directory = Path(root)
+    if not start_directory.is_dir():
         return list()
 
     result = []
-    items = [path for path in root.iterdir()]
+    items = [path for path in start_directory.iterdir()]
     dirs = [path for path in items if path.is_dir()]
     if not dirs:
         files = [path for path in items if path.is_file()]
         if not files:
-            result.append(root)
+            result.append(start_directory)
         elif ignore_empty_files:
             non_empty = any(file.stat().st_size > 0 for file in files)
             if not non_empty:
-                result.append(root)
+                result.append(start_directory)
 
     for child_dir in dirs:
         result.extend(find_empty_dirs(child_dir, ignore_empty_files))
--- shell_tools/git/misc.py
+++ shell_tools/git/misc.py
@@ -6,27 +6,27 @@ from subprocess import Popen
 
 def find_repositories(root: str | PathLike[str], recursive: bool = False) -> list[Path]:
     """
-    Find all git repositories in `root` directory.
+    Find all git repositories in the `root` directory.
 
     The main criterion is the presence of the **.git** directory inside.
     """
-    root = Path(root)
-    if not root.is_dir():
+    start_directory = Path(root)
+    if not start_directory.is_dir():
         return list()
 
     pattern = r"**/.git" if recursive else r"*/.git"
-    return [folder.parent for folder in root.glob(pattern)]
+    return [folder.parent for folder in start_directory.glob(pattern)]
 
 
 def execute_command(*arguments: str) -> list[str]:
-    """Execute `arguments` as the single shell command."""
+    """Execute `arguments` as a single shell command."""
     with Popen(arguments, stdout=PIPE) as shell:
         stdout, _ = shell.communicate()
         return stdout.decode("utf-8").strip().split("\n")
 
 
 def update_repository(path: str | PathLike[str], submodules: bool) -> list[str]:
-    """Pull the latest changes for given repository at `path`."""
+    """Pull the latest changes for the given repository at `path`."""
     output = execute_command("git", "-C", str(path), "pull")
     if submodules:
         output += execute_command("git", "-C", str(path), "submodule", "update", "--init", "--recursive")
